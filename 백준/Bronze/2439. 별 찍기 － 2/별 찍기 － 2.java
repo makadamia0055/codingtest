c92968af2908f6b182a5268a8428a131a69d3218
makadamia0055 <116548922+makadamia0055@users.noreply.github.com>
2022-12-30 00:03:36 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 120 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,42 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))	
+				){
+			
+			int totalNum = Integer.parseInt(br.readLine());
+			
+			for(int i = 0; i< totalNum; i++) {
+				
+				for(int k= totalNum-i-1; k>0; k--) {
+					bw.append(' ');
+				}
+				for(int j = 0; j<=i; j++) {
+					bw.append('*');
+				}
+				bw.newLine();
+				
+			}
+			bw.flush();
+			
+			
+			
+			
+		}catch(Exception e) {
+			
+		}
+		
+		
+		
+	}
+
+}
