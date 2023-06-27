d26d535041e63fbc3e4e95e55181092b42f248f9
makadamia0055 <116548922+makadamia0055@users.noreply.github.com>
2023-06-27 14:20:08 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 9.08 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,6 +1,5 @@
 class Solution {
     public String solution(int[] food) {
-                
                 StringBuilder sb = new StringBuilder();
 		        for(int i = 1; i<food.length; i++){
 		            for(int j = 0; j<food[i]/2; j++){
@@ -9,9 +8,9 @@ class Solution {
 		        }
 		        String str = sb.toString();
 		        String rvStr = sb.reverse().toString();
-		        
-		        
 		        String answer = str + 0 + rvStr;
+		        
+		        
 		        return answer;
     }
 }
