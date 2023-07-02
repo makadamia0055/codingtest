e4b9ce0ad8883a5bff0189d29fc648a910a965a8
makadamia0055 <116548922+makadamia0055@users.noreply.github.com>
2023-07-02 19:44:35 +0900

    [unrated] Title: 수 조작하기 2, Time: 7.69 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(int[] numLog) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i<numLog.length; i++){
+            int gap = numLog[i] - numLog[i-1];
+            switch(gap){
+                    case 1: 
+                    sb.append('w');
+                    break;
+                    case -1: 
+                    sb.append('s');
+                    break;
+                    case 10: 
+                    sb.append('d');
+                    break;
+                    case -10: 
+                    sb.append('a');
+                    break;
+            }
+        }
+        String answer = sb.toString();
+        return answer;
+    }
+}
