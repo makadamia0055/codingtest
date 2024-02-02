477937d76b33a8a33c16281456571de829ee4e12
GSB_pupungee <116548922+makadamia0055@users.noreply.github.com>
2024-02-02 14:39:26 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 22.79 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[] delete_list) {
+        return  Arrays
+                .stream(arr)
+                .filter(i->!Arrays.stream(delete_list).filter(d->d==i).findAny().isPresent())
+                .toArray();
+        
+    }
+}
